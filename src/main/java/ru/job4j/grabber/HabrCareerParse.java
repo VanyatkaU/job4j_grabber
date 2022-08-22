76530c081caeaf37c34cfb824a908564d5e1fee5
VanyatkaU <ivanKa49@ya.ru>
2022-08-22 20:46:56 +0300

    2.1. Преобразование даты [#289476]

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -5,8 +5,11 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import ru.job4j.grabber.utils.DateTimeParser;
+import ru.job4j.grabber.utils.HabrCareerDateTimeParser;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 
 public class HabrCareerParse {
 
@@ -16,6 +19,7 @@ public class HabrCareerParse {
             "%s/vacancies/java_developer", SOURCE_LINK);
 
     public static void main(String[] args) throws IOException {
+        DateTimeParser dateTimeParser = new HabrCareerDateTimeParser();
         Connection connection = Jsoup.connect(PAGE_LINK);
         Document document = connection.get();
         Elements rows = document.select(".vacancy-card__inner");
@@ -23,8 +27,9 @@ public class HabrCareerParse {
             Element titleElement = row.select(".vacancy-card__title").first();
             Element linkElement = titleElement.child(0);
             String vacancyName = titleElement.text();
-            Elements dateTime = row.select(".vacancy-card__date");
-            String vacancyDate = dateTime.text();
+            Element dateTime = row.select(".vacancy-card__date").first();
+            Element linkTime = dateTime.child(0);
+            LocalDateTime vacancyDate = dateTimeParser.parse(linkTime.attr("datetime"));
             String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
             System.out.printf("%s %s%n %s - ", vacancyName, link, vacancyDate);
         });
